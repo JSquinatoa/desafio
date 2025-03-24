e0c14021fb5ec036fcd6fef86780d7c5187b40ea
JSquinatoa <jsquinatoa@uce.edu.ec>
2025-03-23 22:51:40 -0500

    Creación de la clase usuario

--- gestiondetareas.py
+++ gestiondetareas.py
@@ -16,3 +16,24 @@ class Tarea:
         self.titulo = nuevo_titulo
         self.descripcion = nueva_descripcion
         self.fecha_vencimiento = nueva_fecha
+    
+# clase usuario    
+class Usuario:
+    def __init__(self, nombre_usuario, password):
+        self.nombre_usuario = nombre_usuario
+        self.password = password
+        self.tareas = []
+        
+    def agregar_tarea(self, tarea):
+        self.tarea.append(tarea)
+        
+    def eliminar_tarea(self, titulo_tarea):
+        self.tarea = [tarea for tarea in self.tareas if tarea.titulo != titulo_tarea]
+        #recorre toda la lista de tareas hgata llegar a la tarea que tenga el titulo igual al ingresado
+
+    def obtener_tareas(self):
+        return self.tareas
+    
+    
+    
+    
